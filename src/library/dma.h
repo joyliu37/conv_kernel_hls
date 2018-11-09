23a4996a93c4531bce55f6f73d500e01bd1cab22
joyliu37 <joyliu37@sina.com>
2018-11-08 23:14:08 -0800

    Add the padding boundary check when have input tiling

--- src/library/dma.h
+++ src/library/dma.h
@@ -7,17 +7,21 @@
 
 template<typename T, int data_width>
 void Mem2Stream_feature(PackedStencil<T, data_width, 1, 1, 1>* _feature,
-		hls::stream<PackedStencil<T, data_width, 1, 1, 1>> &out, layerPara para,
-		tilingID iter) {
+		hls::stream<PackedStencil<T, data_width, 1, 1, 1>> &out,
+        layerPara para, tilingID iter) {
 //#pragma HLS inline
 
 	Stencil<T, data_width, 1, 1, 1> temp;
-//TODO: put off_beg and off_end into a profile
-	load_feature2Stream: for (int input_y = 0 - iter.tilingIDy;
-			input_y < para.Y_SZ + 1 - iter.tilingIDy; input_y++) {
+
+    //handle the edge case for blocking the feature map
+    const int8_t x_low = -(iter.tilingIDx > 0) * para.Anchor;
+    const int8_t y_low = -(iter.tilingIDy > 0) * para.Anchor;
+    const int8_t x_high = para.X_SZ + (iter.tilingIDx < (para.X_n - 1)) * para.Anchor;
+    const int8_t y_high = para.Y_SZ + (iter.tilingIDy < (para.Y_n - 1)) * para.Anchor;
+
+	load_feature2Stream: for (int input_y = y_low; input_y < y_high; input_y++) {
 #pragma HLS LOOP_TRIPCOUNT max=18
-		for (int input_x = 0 - iter.tilingIDx;
-				input_x < para.X_SZ + 1 - iter.tilingIDx; input_x++) {
+		for (int input_x = x_low; input_x < x_high; input_x++) {
 #pragma HLS LOOP_TRIPCOUNT max=18
 			for (int input_c = 0; input_c < (para.Cin_SZ / data_width); input_c++) {
 #pragma HLS LOOP_TRIPCOUNT max=2
@@ -32,52 +36,8 @@ void Mem2Stream_feature(PackedStencil<T, data_width, 1, 1, 1>* _feature,
 		}
 	}
 }
-/*
-template<typename T, int data_width>
-void Mem2Stream_feature_new(PackedStencil<T, data_width, 1, 1, 1>* _feature,
-		hls::stream<PackedStencil<T, data_width, 1, 1, 1>> &out, layerPara para,
-		tilingID iter) {
-//#pragma HLS inline
 
-    //packed the following parameter into param
-	Stencil<T, data_width, 1, 1, 1> temp;
-    beg1 = -iter.tilingIDy;
-    end1 = Y_SZ + 1 - iter.tilingIDy;
-    blk1 = Y_SZ;
-    ext1 = 0;
-    iter1 = iter.tilingIDy;
-    beg0 = -iter.tilingIDx;
-    end0 = X_SZ + 1 -iter.tilingIDx;
-    blk0 = X_SZ;
-    ext0 = para.width * para.Chin/DATAWIDTH;
-    iter0 = iter.tilingIDx;
-    beg2 = 0;
-    end2 = 1;
-    blk2 = 0;
-    ext2 = 0;
-    iter2 =0;
-    beg3 = 0;
-    end3 = 1;
-    blk3 = 0;
-    ext3 = 0;
-    iter3 = 0;
 
-//TODO: put off_beg and off_end into a profile
-	load_feature2Stream: for (int idx3 = beg3; idx3 < end3; idx3 ++) {
-        for (int idx2 = beg2; idx2 < end2; idx2 ++) {
-			for (int idx1 = beg1; idx1 < end1; idx1 ++) {
-			    for (int idx0 = beg0; idx0 < end0; idx0 ++) {
-#pragma HLS PIPELINE II=1
-				int32_t ddrAddr = idx0 + iter0 * blk0 +\
-                                  (idx1 + iter1 * blk1) * ext0 +\
-                                  (idx2 + iter2 * blk2) * ext0 * ext1 +\
-                                  (idx3 + iter3 * blk3) * ext0 * ext1 * ext2;
-				temp = _feature[ddrAddr];
-				out.write(temp);
-			}
-		}
-	}
-*/
 template<typename T, int data_width>
 void Mem2Stream_weight(
         PackedStencil<T, data_width, 1, 1, 1> *_weight,
