e63c98c770fd686cd49848e5856308280273b408
joyliu37 <joyliu37@sina.com>
2019-07-17 13:52:53 -0700

    update test shell

--- unit_test.sh
+++ unit_test.sh
@@ -1,3 +1,10 @@
+if [ "$#" -ne 1 ]; then
+    echo "cmd template: $0 <testcase>" >&2
+    echo "<network> = IObuf, featurebuf, a" >&2
+    exit 1
+fi
+
+TEST_MODE=$1
 SRC_DIR=src
 TEST_CASE=${SRC_DIR}/hw/test
 
@@ -8,10 +15,17 @@ fi
 
 vivado_hls=/nobackup/xuany/xilinx/Vivado_HLS/2016.4/bin/vivado_hls 
 
-
-for TEST_DIR in `find $TEST_CASE -mindepth 1 -type d -exec basename {} \;`
-do
+if [ "$TEST_MODE" == "a" ]; then
+    echo "Test all test case."
+    for TEST_DIR in `find $TEST_CASE -mindepth 1 -type d -exec basename {} \;`
+    do
+        echo "Test $TEST_DIR."
+        $vivado_hls -f unit_test.tcl -tclargs $SRC_DIR $TEST_DIR
+        echo "Finish test $TEST_DIR"
+    done
+else
+    TEST_DIR=${TEST_MODE}
     echo "Test $TEST_DIR."
     $vivado_hls -f unit_test.tcl -tclargs $SRC_DIR $TEST_DIR
     echo "Finish test $TEST_DIR"
-done
+fi
